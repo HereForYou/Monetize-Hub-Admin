d9064c19d82aa6b9ffed1fd694669a3c93b9ae41
HereForYou <lucaslee.dev.infor@gmail.com>
2024-10-21 09:10:04 +0900

    feat: add delete button, apply responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { ENDPOINT } from "./data";
 import "./App.css";
 import axios from "axios";
+import { X } from "lucide-react";
 
 function App() {
   const [setting, setSetting] = useState<any>({});
@@ -33,14 +34,18 @@ function App() {
   };
 
   const addVideo = async () => {
-    axios.post(ENDPOINT + "/api/setting/add", newVideo).then((res) => {
-      console.log("This is add response", res.data);
-      setSetting(res.data);
-      setNewVideo({
-        title: "",
-        link: "",
+    if (newVideo.link !== "" && newVideo.title !== "") {
+      axios.post(ENDPOINT + "/api/setting/add", newVideo).then((res) => {
+        console.log("This is add response", res.data);
+        setSetting(res.data);
+        setNewVideo({
+          title: "",
+          link: "",
+        });
       });
-    });
+    } else {
+      setError("Fill all fields!");
+    }
   };
 
   const onChangePassword = (e: any) => {
@@ -52,59 +57,74 @@ function App() {
     else setError("Invalid password");
   };
 
+  const removeVideo = (index: number) => {
+    axios.post(ENDPOINT + "/api/setting/remove", { index: index }).then((res) => {
+      console.log("This is add response", res.data);
+      setSetting(res.data);
+    });
+  };
+
   return (
-    <div className='w-screen p-10 h-screen'>
+    <div className='w-full sm:p-10 p-2 h-screen'>
       {isVerified ? (
-        <table className='w-full border border-slate-700 p-10'>
-          <thead className='w-full'>
-            <tr className='w-full'>
-              <th className='text-lg w-1/12 p-2'>Index</th>
-              <th className='text-lg w-3/12 text-left pl-2'>Title</th>
-              <th className='text-lg w-7/12'>Link</th>
-              <th className='text-lg w-full'>Link</th>
-            </tr>
-          </thead>
-          <tbody className='w-full'>
-            {setting?.taskList?.map((item: any, index: number) => (
-              <tr className='w-full odd:bg-slate-200' key={item.id}>
-                <td className='text-lg text-center w-1/12 p-2'>{index + 1}</td>
-                <td className='text-lg text-left w-3/12 pl-2'>{item?.title}</td>
-                <td className='text-lg text-left w-7/12'>
-                  <a href={item?.link}>{item?.link}</a>
-                </td>
-                <td className='text-lg text-left'></td>
-                {/* <img src={item.image} alt={item.id} /> */}
+        <div className=''>
+          <table className='w-full border border-slate-700'>
+            <thead className=''>
+              <tr className=''>
+                <th className='text-lg w-1/12 p-2 hidden sm:block'>Index</th>
+                <th className='text-lg w-3/12 text-left pl-2'>Title</th>
+                <th className='text-lg w-8/12 sm:w-7/12 pl-2'>Link</th>
+                <th className='text-lg w-1/12'></th>
               </tr>
-            ))}
-            <tr className='w-full odd:bg-slate-200'>
-              <td className='text-2xl font-extrabold text-center p-2'>+</td>
-              <td className='text-lg text-left w-3/12 px-2'>
-                <input
-                  className='focus:w-full border border-slate-500 py-1 rounded-lg w-1/2 transition-all duration-300'
-                  value={newVideo.title}
-                  name='title'
-                  onChange={(e) => setNewVideo({ ...newVideo, [e.target.name]: e.target.value })}
-                />
-              </td>
-              <td className='text-lg text-left w-7/12'>
-                <input
-                  className='focus:w-full border border-slate-500 py-1 rounded-lg w-2/3 transition-all duration-300'
-                  value={newVideo.link}
-                  name='link'
-                  onChange={(e) => setNewVideo({ ...newVideo, [e.target.name]: e.target.value })}
-                />
-              </td>
-              <td className='text-lg w-1/12 text-center py-2'>
-                <button className='py-1 bg-green-500 text-white' onClick={() => addVideo()}>
-                  Add
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+            </thead>
+            <tbody className='w-full'>
+              {setting?.taskList?.map((item: any, index: number) => (
+                <tr className='w-full odd:bg-slate-200 text-sm sm:text-lg' key={item.id}>
+                  <td className='text-center w-1/12 p-2 hidden sm:block'>{index + 1}</td>
+                  <td className='text-left w-3/12 pl-2'>{item?.title}</td>
+                  <td className='text-left w-8/12 sm:w-7/12 pl-2 break-words'>
+                    <a href={item?.link}>{item?.link}</a>
+                  </td>
+                  <td
+                    className='text-left w-1/12 text-red-400 font-extrabold hover:text-red-800'
+                    onClick={() => removeVideo(index)}>
+                    <X size={16} />
+                  </td>
+                  {/* <img src={item.image} alt={item.id} /> */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className='flex gap-5 justify-center items-center mt-2 w-full'>
+            <p>Title</p>
+            <input
+              className='focus:w-full border border-slate-500 py-1 rounded-lg transition-all duration-300 w-full pl-2'
+              value={newVideo.title}
+              name='title'
+              onChange={(e) => setNewVideo({ ...newVideo, [e.target.name]: e.target.value })}
+            />
+          </div>
+          <div className='flex gap-5 justify-center items-center mt-2 w-full'>
+            <p>Link</p>
+            <input
+              className='focus:w-full border border-slate-500 py-1 rounded-lg w-full transition-all duration-300 pl-2'
+              value={newVideo.link}
+              name='link'
+              onChange={(e) => setNewVideo({ ...newVideo, [e.target.name]: e.target.value })}
+            />
+          </div>
+          <button className='py-1 bg-green-500 text-white w-full mt-2' onClick={() => addVideo()}>
+            Add
+          </button>
+          {error !== "" && (
+            <p className='bg-red-500 bg-opacity-30 border border-red-700 px-32 py-2 mt-2 flex justify-center'>
+              {error}
+            </p>
+          )}
+        </div>
       ) : (
         <div className='w-full h-full justify-center items-center flex flex-col gap-5'>
-          <div className='flex gap-10 justify-center items-center'>
+          <div className='flex gap-10 justify-center items-center sm:flex-row flex-col'>
             <p>Password</p>
             <input
               name='password'
@@ -115,7 +135,9 @@ function App() {
             />
             <button onClick={() => joinAsAdmin()}>Join</button>
           </div>
-          {error !== "" && <p className="bg-red-500 bg-opacity-30 border border-red-700 px-32 py-2">{error}</p>}
+          {error !== "" && (
+            <p className='bg-red-500 bg-opacity-30 border border-red-700 px-32 py-2 flex justify-center'>{error}</p>
+          )}
         </div>
       )}
     </div>
